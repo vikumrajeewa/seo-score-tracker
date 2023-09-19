3b6665be3c92d67a565b0d096d466b6448574cb2
vikum <vikumrp@gmail.com>
2023-09-19 14:17:53 +0530

    article-content

---
+++ single.php
@@ -0,0 +1,7 @@
+ <!-- Your article sol depth -->
+ <div class="article-content">
+  <!-- Your article content goes here -->
+</div>
+<div class="scroll-depth">
+  Scroll Depth: <?php echo get_post_meta(get_the_ID(), 'scroll_depth', true); ?>%
+</div>

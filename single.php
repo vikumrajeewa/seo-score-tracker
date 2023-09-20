29d6c2c2b4e0c5391ff0c2012de978b32549f6ad
vikum <vikumrp@gmail.com>
2023-09-20 22:47:07 +0530

    add new session %

--- single.php
+++ single.php
@@ -5,3 +5,8 @@
 <div class="scroll-depth">
   Scroll Depth: <?php echo get_post_meta(get_the_ID(), 'scroll_depth', true); ?>%
 </div>
+
+<div class="new-session-percentage">
+  New Session Percentage: <?php echo get_post_meta(get_the_ID(), 'new_session_percentage', true); ?>%
+</div>
+
